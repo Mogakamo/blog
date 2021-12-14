ad5f8fc7d3333b0ba0c1db61630b4a51a73a5f7d
mogakamo <mogaka.amo254@gmail.com>
2021-12-14 13:33:02 +0300

    commit - but tailwind is not set

---
+++ components/Common/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div className="bg-blue-500 p-6">
+            This is the Navbar
+        </div>
+    )
+}
+
+export default NavBar
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import NavBar from '../components/Common/NavBar'
+
 
 export default function Home() {
   return (
     <>
-    This is my blog...
+      <div>
+        <NavBar />
+      </div>
     </>
   )
 }
